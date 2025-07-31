41ef93ff624fdeb62b2ed66fbb8f23666e82a651
Ajay <ajay@oraclelinux7.9>
2025-07-31 15:42:07 +0530

    Second shell script

---
+++ 2file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+var="Go to bed"
+
+echo $var
+echo "Knowedge is power"
+echo "Hello $USER"
+echo "Today is \c"; date
+echo "Number of user login : \c"; who | wc -l
+echo "calender"
+cal
+echo "date"
+date
+
